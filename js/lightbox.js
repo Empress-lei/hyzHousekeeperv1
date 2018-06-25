a57de9ea54249978db104263472d793f718eb5e9
wubeilei <m18235580527@163.com>
2018-06-25 11:44:42 +0800

    样式调整

--- js/lightbox.js
+++ js/lightbox.js
@@ -23,27 +23,27 @@ function initMyImgShow(){
 		myImg.src = $(this).attr("src");
 		var bg = "";
 		bg += '<div id="popup" class="popup" onclick="closeMyImg(this)">';
-		bg += '<div class="bg"><img  src="'+myImg.src+'" alt="" width="'+screen.width+'"/></div>';
+		bg += '<div class="bg"><img  src="'+myImg.src+'" alt="" max-width="'+screen.width+'"/></div>';
 		bg += '</div>';
 		//$(this).parent().after(bg);
 		$("body").append(bg); 
 		//当前屏幕举例顶部的高度 document.body.scrollTop
 		//网页可见的高度  document.body.clientHeight
 		//添加css样式，设置图片定位
-		$("#popup").css({"position":"fixed","width":"100%","max-width":"540px","background-color" : "rgba(0,0,0,0.5)","height":window.screen.height+78,"overflow" : " auto"});
-		$("#popup").css({"top":"0%","margin-top":"-50px"});
-		$("#popup").css({"z-index":"999"});
+		// $("#popup").css({});
+		// $("#popup").css({"top":"0%","margin-top":"-50px"});
+		// $("#popup").css({"z-index":"999"});
 		//$("#popup>.bg>img").css({"height":window.screen.height/2});
 		//调整居中
-		var topHeight = (window.screen.height-$(".bg:first>img").height())/2;
-		if(topHeight < 0  ){
-			//全屏显示
-			$("#popup>.bg").css({"margin-top":"13%"});
-			$("#popup>.bg>img").css({"width":"100%"})
-		}else{
-			//水平居中
-			$("#popup>.bg").css({"padding-top":topHeight+"px"});
-		}
+		// var topHeight = (window.screen.height-$(".bg:first>img").height())/2;
+		// if(topHeight < 0  ){
+		// 	//全屏显示
+		// 	$("#popup>.bg").css({"margin-top":"13%"});
+		// 	$("#popup>.bg>img").css({"width":"100%"})
+		// }else{
+		// 	//水平居中
+		// 	$("#popup>.bg").css({"padding-top":topHeight+"px"});
+		// }
 		//取消淡入淡出效果，由于其他定位影响显示
 		//设置img  style="display:none;"
 		//$("#popup>.bg>img").fadeIn("slow");
